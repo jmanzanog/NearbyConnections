738dbbb51557f970ce05a1e2adb9071ebfb39ba5
josemanzano <j.manzano.g@hotmail.com>
2019-06-26 22:28:59 -0500

    Suscripción para control remoto del LED

--- mqtt/src/main/java/org/example/mqtt/MainActivity.java
+++ mqtt/src/main/java/org/example/mqtt/MainActivity.java
@@ -4,10 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManager;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.io.IOException;
 
 /**
  * Skeleton of an Android Things activity.
@@ -28,31 +36,97 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
  *
  * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements MqttCallback {
     private static final String TAG = "Things";
-    private static final String topic = "jmanzanog/test";
-    private static final String hello = "Hello world!";
+    private final String PIN_LED = "BCM18";
+    public Gpio mLedGpio;
+    private static final String topic_gestion = "jmanzanog/gestion";
+    private static final String topic_led = "jmanzanog/led";
+    static final String hello = "Hello world! Android Things conectada.";
     private static final int qos = 1;
     private static final String broker = "tcp://iot.eclipse.org:1883";
-    private static final String clientId = "Test134568789";
+    MqttClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PeripheralManager service = PeripheralManager.getInstance();
         try {
-            MqttClient client = new MqttClient(broker, clientId, new MemoryPersistence());
+            mLedGpio = service.openGpio(PIN_LED);
+            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+        } catch (IOException e) {
+            Log.e(TAG, "Error en el API PeripheralIO", e);
+        }
+        try {
+            String clientId = MqttClient.generateClientId();
+            client = new MqttClient(broker, clientId, new MemoryPersistence());
+            client.setCallback(this);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            connOpts.setKeepAliveInterval(60);
+            connOpts.setWill(topic_gestion, "Android Things desconectada!".getBytes(), qos, false);
             Log.i(TAG, "Conectando al broker " + broker);
-            client.connect();
+            client.connect(connOpts);
             Log.i(TAG, "Conectado");
             Log.i(TAG, "Publicando mensaje: " + hello);
             MqttMessage message = new MqttMessage(hello.getBytes());
             message.setQos(qos);
-            client.publish(topic, message);
+            client.publish(topic_gestion, message);
             Log.i(TAG, "Mensaje publicado");
-            client.disconnect();
-            Log.i(TAG, "Desconectado");
+            client.subscribe(topic_led, qos);
+            Log.i(TAG, "Suscrito a " + topic_led);
         } catch (MqttException e) {
             Log.e(TAG, "Error en MQTT.", e);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            if (client != null && client.isConnected()) {
+                client.disconnect();
+            }
+        } catch (MqttException e) {
+            Log.e(TAG, "Error en MQTT.", e);
+        }
+        if (mLedGpio != null) {
+            try {
+                mLedGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error en el API PeripheralIO", e);
+            } finally {
+                mLedGpio = null;
+            }
+        }
+    }
+
+    @Override
+    public void connectionLost(Throwable cause) {
+        Log.d(TAG, "Conexión perdida...");
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        String payload = new String(message.getPayload());
+        Log.d(TAG, payload);
+        switch (payload) {
+            case "ON":
+                mLedGpio.setValue(true);
+                Log.d(TAG, "LED ON!");
+                break;
+            case "OFF":
+                mLedGpio.setValue(false);
+                Log.d(TAG, "LED OFF!");
+                break;
+            default:
+                Log.d(TAG, "Comando no soportado");
+                break;
+        }
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+        Log.d(TAG, "Entrega completa!");
     }
 }
